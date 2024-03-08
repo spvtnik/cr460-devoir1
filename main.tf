bc93fb68a624ef32792ab7fb2b3112c0b2e8c2d3
spvtnik <mtrudel@hotmail.com>
2024-03-08 15:54:16 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "cr460-resources"
   location = "West Europe"
 }
 
 resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+  name                = "cr460-network"
   address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.example.location
   resource_group_name = azurerm_resource_group.example.name
