a4b68a36676d21a731677883cb1261b092dd6181
spvtnik <mtrudel@hotmail.com>
2024-03-12 19:11:21 -0400

    commit

--- providers.tf
+++ providers.tf
@@ -19,4 +19,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+  subscription_id = var.subscriptionId
+  client_id       = var.clientId
+  client_secret   = var.clientSecret
+  tenant_id       = var.tenantId
 }
