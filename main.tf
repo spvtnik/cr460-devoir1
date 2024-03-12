f052a4309470c8aa5f24ee585a7b24cffc7eb3ff
spvtnik <mtrudel@hotmail.com>
2024-03-12 19:38:30 -0400

    commit

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
-resource "random_pet" "rg_name" {
-  prefix = var.resource_group_name_prefix
-}
-
 resource "azurerm_resource_group" "rg" {
   location = var.resource_group_location
-  name     = random_pet.rg_name.id
+  name     = "cr460-devoir1"
 }
 
 # Create virtual network
